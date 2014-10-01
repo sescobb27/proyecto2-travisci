e36529cdd4c3cb68dfdc4ac6018a3593f66cc282
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-10-01 14:47:10 -0500

    Seeding DB before testing

--- models/category_test.go
+++ models/category_test.go
@@ -5,37 +5,7 @@ import (
         "testing"
 )
 
-var (
-        categories = []string{"Pasta", "Carne", "Pollo", "Ensalada", "Desayuno",
-                "Almuerzo", "Postre", "Sopa", "Vegetariana", "Menu Infantil",
-                "Comida Rapida", "Almuerzo para 2", "Desayuno para 2", "Comida para 2",
-                "Ensalada de Frutas", "Gourmet"}
-        descriptions = []string{"Ricas Pasta", "Ricas Carnes", "Rico Pollo",
-                "Ricas Ensaladas", "Ricos Desayunos", "Ricos Almuerzos", "Ricos Postres",
-                "Ricas Sopas", "Rica Comida Vegetariana", "Ricos Menu Infantil",
-                "Rica Comida Rapida", "Ricos Almuerzo para 2", "Ricos Desayuno para 2",
-                "Ricas Comidas para 2", "Ricas Ensaladas de Frutas", "Ricas Comida Gourmet"}
-)
-
-func insert_Categories() {
-        for i, category := range categories {
-                c := &Category{Name: category,
-                        Description: descriptions[i]}
-                c.Create()
-        }
-}
-
-func restoreCategories(t *testing.T) {
-        db, err := stablishConnection()
-        assert.NoError(t, err)
-        _, err = db.Exec("truncate table categories restart identity CASCADE")
-        assert.NoError(t, err)
-        db.Close()
-}
-
 func TestGetCategories(t *testing.T) {
-        insert_Categories()
         categories := GetCategories()
         assert.NotEmpty(t, categories)
-        restoreCategories(t)
 }
--- models/location_test.go
+++ models/location_test.go
@@ -5,32 +5,9 @@ import (
         "testing"
 )
 
-var (
-        locations = []string{"Poblado", "Laureles", "Envigado", "Caldas",
-                "Sabaneta", "Colores", "Estadio", "Calazans", "Bello", "Boston",
-                "Prado Centro", "Itagui", "Belen", "Guayabal"}
-)
-
-func insert_Locations() {
-        for _, location := range locations {
-                l := &Location{Name: location}
-                l.Create()
-        }
-}
-
-func restoreLocations(t *testing.T) {
-        db, err := stablishConnection()
-        assert.NoError(t, err)
-        _, err = db.Exec("truncate table locations restart identity CASCADE")
-        assert.NoError(t, err)
-        defer db.Close()
-}
-
 func TestGetLocations(t *testing.T) {
-        insert_Locations()
         locations := GetLocations()
         if len(locations) == 0 {
                 t.Fatal("Locations should not be empty")
         }
-        restoreLocations(t)
 }
